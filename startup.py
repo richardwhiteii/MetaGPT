7c40231791648c7ad5d3f822df120c78a6682708
Richard <richard@me>
2023-09-09 14:01:17 +0000

    aider: The `main` function now uses the `--prompt` or `-p` flag to provide the idea variable.
    # Aider chat conversation:
    
    USER: I want the idea variable to use the flag --prompt or -p
    
    ASSISTANT: To achieve this, we need to modify the `main` function in the `startup.py` file. We will use the `fire` library's `Fire` function to create a command line interface (CLI). This function takes a dictionary where the keys are the command line flags and the values are the corresponding function parameters.
    
    Here is the *edit block* for the change:
    
    ```python
    startup.py
    <<<<<<< HEAD
    def main(
        idea: str,
        investment: float = 3.0,
        n_round: int = 5,
        code_review: bool = True,
        run_tests: bool = False,
        implement: bool = True
    ):
        """
        We are a software startup comprised of AI. By investing in us,
        you are empowering a future filled with limitless possibilities.
        :param idea: Your innovative idea, such as "Creating a snake game."
        :param investment: As an investor, you have the opportunity to contribute
        a certain dollar amount to this AI company.
        :param n_round:
        :param code_review: Whether to use code review.
        :return:
        """
        if platform.system() == "Windows":
            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
        asyncio.run(startup(idea, investment, n_round,
                    code_review, run_tests, implement))
    
    if __name__ == '__main__':
        fire.Fire(main)
    =======
    def main(
        prompt: str,
        investment: float = 3.0,
        n_round: int = 5,
        code_review: bool = True,
        run_tests: bool = False,
        implement: bool = True
    ):
        """
        We are a software startup comprised of AI. By investing in us,
        you are empowering a future filled with limitless possibilities.
        :param prompt: Your innovative idea, such as "Creating a snake game."
        :param investment: As an investor, you have the opportunity to contribute
        a certain dollar amount to this AI company.
        :param n_round:
        :param code_review: Whether to use code review.
        :return:
        """
        if platform.system() == "Windows":
            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
        asyncio.run(startup(prompt, investment, n_round,
                    code_review, run_tests, implement))
    
    if __name__ == '__main__':
        fire.Fire({
            'p': main,
            '--prompt': main
        })
    >>>>>>> updated
    ```
    
    This change will allow you to use the `--prompt` or `-p` flag when running the script from the command line to provide the idea.

--- startup.py
+++ startup.py
@@ -41,7 +41,7 @@ async def startup(
 
 
 def main(
-    idea: str,
+    prompt: str,
     investment: float = 3.0,
     n_round: int = 5,
     code_review: bool = True,
@@ -51,7 +51,7 @@ def main(
     """
     We are a software startup comprised of AI. By investing in us,
     you are empowering a future filled with limitless possibilities.
-    :param idea: Your innovative idea, such as "Creating a snake game."
+    :param prompt: Your innovative idea, such as "Creating a snake game."
     :param investment: As an investor, you have the opportunity to contribute
     a certain dollar amount to this AI company.
     :param n_round:
@@ -60,10 +60,13 @@ def main(
     """
     if platform.system() == "Windows":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(startup(idea, investment, n_round,
+    asyncio.run(startup(prompt, investment, n_round,
                 code_review, run_tests, implement))
 
 
 if __name__ == '__main__':
-    fire.Fire(main)
+    fire.Fire({
+        'p': main,
+        '--prompt': main
+    })
     
