90798b7e37eb78054e842c49410623d8097585fa
Richard <richard@me>
2023-09-09 14:07:40 +0000

    aider: Maintain the `idea` variable and use a new variable `prompt_content` to hold the content of the prompt in the `main` function.
    # Aider chat conversation:
    
    USER: the variable "idea" should be maintained and not replaced with the string prompt.  The larger application is still depending on using the idea value to revisit the initial idea.
    
    ASSISTANT: Understood. We will modify the `main` function in the `startup.py` file to maintain the `idea` variable and use a new variable `prompt_content` to hold the content of the prompt, whether it's a file path or a string.
    
    Here is the *edit block* for the changes:
    
    ```python
    startup.py
    <<<<<<< HEAD
    def main(
        prompt: str,
        investment: float = 3.0,
        n_round: int = 5,
        code_review: bool = True,
        run_tests: bool = False,
        implement: bool = True
    ):
        """
        We are a software startup comprised of AI. By investing in us,
        you are empowering a future filled with limitless possibilities.
        :param prompt: Your innovative idea, such as "Creating a snake game."
        :param investment: As an investor, you have the opportunity to contribute
        a certain dollar amount to this AI company.
        :param n_round:
        :param code_review: Whether to use code review.
        :return:
        """
        # Check if the prompt is a file path
        if os.path.isfile(prompt):
            # If it is, read the file and use its content as the idea
            with open(prompt, 'r') as file:
                idea = file.read()
        else:
            # If it's not a file path, use the prompt as the idea directly
            idea = prompt
    
        if platform.system() == "Windows":
            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
        asyncio.run(startup(idea, investment, n_round,
                    code_review, run_tests, implement))
    =======
    def main(
        idea: str,
        investment: float = 3.0,
        n_round: int = 5,
        code_review: bool = True,
        run_tests: bool = False,
        implement: bool = True
    ):
        """
        We are a software startup comprised of AI. By investing in us,
        you are empowering a future filled with limitless possibilities.
        :param idea: Your innovative idea, such as "Creating a snake game."
        :param investment: As an investor, you have the opportunity to contribute
        a certain dollar amount to this AI company.
        :param n_round:
        :param code_review: Whether to use code review.
        :return:
        """
        # Check if the idea is a file path
        if os.path.isfile(idea):
            # If it is, read the file and use its content as the prompt_content
            with open(idea, 'r') as file:
                prompt_content = file.read()
        else:
            # If it's not a file path, use the idea as the prompt_content directly
            prompt_content = idea
    
        if platform.system() == "Windows":
            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
        asyncio.run(startup(prompt_content, investment, n_round,
                    code_review, run_tests, implement))
    >>>>>>> updated
    ```
    This change will ensure that the `idea` variable is maintained and a new variable `prompt_content` is used to hold the content of the prompt.

--- startup.py
+++ startup.py
@@ -43,7 +43,7 @@ async def startup(
 import os
 
 def main(
-    prompt: str,
+    idea: str,
     investment: float = 3.0,
     n_round: int = 5,
     code_review: bool = True,
@@ -53,25 +53,25 @@ def main(
     """
     We are a software startup comprised of AI. By investing in us,
     you are empowering a future filled with limitless possibilities.
-    :param prompt: Your innovative idea, such as "Creating a snake game."
+    :param idea: Your innovative idea, such as "Creating a snake game."
     :param investment: As an investor, you have the opportunity to contribute
     a certain dollar amount to this AI company.
     :param n_round:
     :param code_review: Whether to use code review.
     :return:
     """
-    # Check if the prompt is a file path
-    if os.path.isfile(prompt):
-        # If it is, read the file and use its content as the idea
-        with open(prompt, 'r') as file:
-            idea = file.read()
+    # Check if the idea is a file path
+    if os.path.isfile(idea):
+        # If it is, read the file and use its content as the prompt_content
+        with open(idea, 'r') as file:
+            prompt_content = file.read()
     else:
-        # If it's not a file path, use the prompt as the idea directly
-        idea = prompt
+        # If it's not a file path, use the idea as the prompt_content directly
+        prompt_content = idea
 
     if platform.system() == "Windows":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(startup(idea, investment, n_round,
+    asyncio.run(startup(prompt_content, investment, n_round,
                 code_review, run_tests, implement))
 
 
